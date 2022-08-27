480253736438ce92382acd953ee42d697aa40ce7
ksera524 <ksera631@gmail.com>
2022-08-27 23:08:34 +0900

    abc266

--- abc266/src/bin/a.rs
+++ abc266/src/bin/a.rs
@@ -5,13 +5,5 @@ fn main() {
     input! {
         s:Chars,
     }
-    if s.len() == 1{
-        println!("{}",s[0]);
-        return ;
-    }
-    if  s.len() % 2 == 1
     {println!("{}",s[s.len() /2 ])} 
-    else {println!("{}",s[s.len() /2])}
-    
-    
 }
